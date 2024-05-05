08175e0eb1384c5b56d5b8b822250bad63f01f07
Lim Hong Han <limhonghan1987@gmail.com>
2024-05-05 19:55:23 +0800

    Update: Added FFI testing

---
+++ test/FFI.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License_Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+
+contract FFITest is Test {
+    function testFFI() public {}
+}
